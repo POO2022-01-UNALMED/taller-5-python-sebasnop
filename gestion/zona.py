15ef326e6fc99b1330ed91eb5f9a60e25a03eceb
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2022-05-05 01:57:22 -0500

    Completando Zoologico y Zona

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,4 +1,30 @@
+from zooAnimales.animal import Animal
+
 class Zona:
+    
     def __init__(self, nombre, zoo = None):
         self._nombre = nombre
         self._zoo = zoo
+        self._animales=[]
+
+    def agregarAnimales(self,animal):
+        self._animales.append(animal)
+
+    def cantidadAnimales(self):
+        x = 0
+        
+        for i in self._animales:
+            x += 1
+        return x 
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self,nombre):
+        self,nombre=nombre
+
+    def setZoo(self,zoo):
+        self._zoo=zoo
+
+    def getZoo(self):
+        return self._zoo
--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,4 +1,32 @@
+from gestion.zona import Zona
+
 class Zoologico:
+    
     def __init__(self, nombre, ubicacion):
         self._nombre = nombre
         self._ubicacion = ubicacion
+        self._zonas=[]
+    
+    def agregarZonas(self,zonas):
+        self._zonas.append(zonas)
+
+    def cantidadTotalAnimales(self):
+        Nzonas=0
+
+        for i in self._zonas:
+            Nzonas+=i.cantidadAnimales()
+        
+        return Nzonas
+      
+    
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self,nombre):
+        self._nombre=nombre
+
+    def getUbicacion(self):
+         return self._ubicacion
+    
+    def getZona(self):
+        return self._zonas
